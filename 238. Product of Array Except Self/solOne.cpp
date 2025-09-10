d4f1d62972f85d6dfb4b1a2f74ad3660b93dd47c
vivek chitturi <139134664+thecodingvivek@users.noreply.github.com>
2025-09-10 18:50:20 +0530

    sep10

---
+++ 238. Product of Array Except Self/solOne.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "../printVector/printvector.cpp"
+
+
+int main(){
+    std::vector<int> nums = {-1,1,0,-3,3};
+    int p1=1;
+    int p2=1;
+    int n=nums.size();
+    std::vector<int> output(n,1);
+
+
+    for(int i=0;i<n;i++)
+    {
+        output[i]*=p1;
+        p1*=nums[i];
+        int j=n-i-1;
+        output[j]*=p2;
+        p2*=nums[j];
+    }
+}
