21a9c62c96cdb2571661def23c32634b99c904e8
vivek chitturi <139134664+thecodingvivek@users.noreply.github.com>
2025-09-15 22:16:41 +0530

    spiral matrix

--- 54. Spiral Matrix/solOne.cpp
+++ 54. Spiral Matrix/solOne.cpp
@@ -3,7 +3,7 @@
 #include "../printVector/printvector.cpp"
 
 int main(){
-    std::vector<std::vector<int>> matrix = {{1,2,3,4},{5,6,7,8}};
+    std::vector<std::vector<int>> matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
     std::vector<int> result;
     int start=0;
     int end=0;
@@ -16,22 +16,27 @@ int main(){
     while(start==end){
         if(!horizontalrev)
         {
+            std::cout<<"joo";
             result.insert(result.end(), matrix[pointerv].begin()+start, matrix[pointerv].end()-end);
             pointerh=matrix[pointerv].size()-end;
-            for(int i=pointerv;i<matrix.size()-end;i++)
+            for(int i=pointerv+1;i<matrix.size()-end;i++)
             {
-                result.push_back(matrix[pointerv][matrix[pointerv].size()-1]);
+                result.push_back(matrix[i][matrix[pointerv].size()-1]);
             }
+            std::cout<<"got this";
             pointerv=matrix.size()-end;
         }
         else{
+            std::cout<<"koo";
             result.insert(result.end(), matrix[pointerv].rbegin()+start, matrix[pointerv].rend()-end);
+            printVector(result);
+            std::cout<<"lol";
             pointerh=start;
             start++;
             end++;
-            for(int i=pointerv;i>=start;i--)
+            for(int i=pointerv-1;i>=start;i--)
             {
-                result.push_back(matrix[pointerv][matrix[pointerv].size()-1]);
+                result.push_back(matrix[i][matrix[pointerv].size()-1]);
             }
             pointerv=start;
         }
@@ -40,5 +45,4 @@ int main(){
     }
 
 
-    printVector(result);
 }
