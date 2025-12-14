6fa1ce694d7acffc33e0159b6faa3a0a84922fe1
vivek chitturi <139134664+thecodingvivek@users.noreply.github.com>
2025-12-14 12:18:13 +0530

    solved 229

--- 229. Majority Element II/solOne.py
+++ 229. Majority Element II/solOne.py
@@ -1,1 +1,40 @@
-nums = [3,2,3]
+nums = [3,3,2,2,2,3]
+elemf = 0
+elems=0
+
+countf=0
+counts=0
+
+for i in range(len(nums)):
+    if elemf == nums[i]:
+        countf+=1
+    elif elems == nums[i]:
+        counts +=1
+    
+    elif countf == 0:
+        elemf = nums[i]
+        countf = 1
+    elif counts == 0:
+        elems = nums[i]
+        counts = 1
+    else:
+        countf-=1
+        counts-=1
+
+
+co=0
+cs=0
+finall = []
+for j in nums:
+    if j == elemf:
+        co+=1
+    elif j == elems:
+        cs+=1
+    else:
+        pass
+
+if co > len(nums)//3:
+    finall.append(elemf)
+if cs > len(nums)//3:
+    finall.append(elems)
+print(finall)
