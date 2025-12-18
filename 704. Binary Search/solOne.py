89cb73ede6684876a7989f331f312026cf267517
vivek chitturi <139134664+thecodingvivek@users.noreply.github.com>
2025-12-18 20:48:38 +0530

    solved 704

---
+++ 704. Binary Search/solOne.py
@@ -0,0 +1,14 @@
+nums = [-1,0,3,5,9,12]
+target = 9
+i = 0
+j = len(nums)-1
+while i<=j:
+    mid  = (i+j)//2
+    if nums[mid] == target:
+        print(mid)
+        break
+    elif nums[mid] > target:
+        j = mid - 1
+    else:
+        i = mid+1
+    
